608bcba734349ee8d60eda059ecb905c5edb0ae9
Melasmar-711 <Mahmoudelasmar2000@outlook.com>
2025-04-24 12:14:54 +0200

    started designing tests for action planning

---
+++ src/cooking_manager/tests/test_action_planing.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import rospy
+import unittest
+import random
+
+
+class TestActionPlanning(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        rospy.init_node('test_action_planning', anonymous=True)
+
+        
+
+    def test_parsing_step(self): # the default number of numbers used is 10
+        
+
+
+if __name__ == "__main__":
+    import rostest
+    rostest.rosrun('example_pkg', 'test_add_two_ints_server',
+                   TestAddTwoIntsServer)
