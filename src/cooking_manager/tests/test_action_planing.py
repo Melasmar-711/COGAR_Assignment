73f64d29e9210e1ac6c42278692868801140cfc3
Melasmar-711 <Mahmoudelasmar2000@outlook.com>
2025-04-25 22:30:23 +0200

    added another test cheking the resequencing of actions when a valid command is passed

--- src/cooking_manager/tests/test_action_planing.py
+++ src/cooking_manager/tests/test_action_planing.py
@@ -12,19 +12,23 @@ class TestActionPlanning(unittest.TestCase):
 
         self.dummy_steps=["cut the onions","cook onion","boil the water"]
 
-        self.dummy_action_seqences=[
+        self.dummy_action_seqences_no_valid_commands_sent=[
                                     ["grab the onions urgent", "cut the onions not_urgent"],
                                     ["get onion not_urgent", "put in pan not_urgent", "turn on low heat not_urgent"],
                                     ["grab the pan not_urgent", "fill the pan with water urgent", "put the pan on the stove urgent"]
-                                    
                                     ]
-        
+        self.dummy_action_seqences_valid_commands_sent=[
+                                    ["grab the onions urgent", "cut the onions not_urgent"],
+                                    ["wait 2 seconds","get onion not_urgent", "put in pan not_urgent", "turn on low heat not_urgent"],
+                                    ["grab the pan not_urgent", "fill the pan with water urgent", "put the pan on the stove urgent"]
+                                    ]      
 
 
 
 
     def setUp(self):
         self.step_publisher = rospy.Publisher('recipe_step', RecipeStep, queue_size=10)
+        self.valid_command_publisher = rospy.Publisher('valid_command', String, queue_size=10)
         self.rate= rospy.Rate(10)  # 10 Hz
         self.Subscriber=rospy.Subscriber('action_sequence',String, self.sequence_callback)
 
@@ -58,15 +62,45 @@ class TestActionPlanning(unittest.TestCase):
             
             self.rate.sleep()
 
-
-
         # Check if the received sequences match the dummy action sequences
-        if len(self.received_sequences) == len(self.dummy_action_seqences):
-            for received, expected in zip(self.received_sequences, self.dummy_action_seqences):
+        if len(self.received_sequences) == len(self.dummy_action_seqences_no_valid_commands_sent):
+            for received, expected in zip(self.received_sequences, self.dummy_action_seqences_no_valid_commands_sent):
                 self.assertEqual(received, expected)
         else:
-            self.fail(f"Number of received sequences ({len(self.received_sequences)}) does not match expected ({len(self.dummy_action_seqences)}).")
+            self.fail(f"Number of received sequences ({len(self.received_sequences)}) does not match expected ({len(self.dummy_action_seqences_no_valid_commands_sent)}).")
+        
+        rospy.loginfo("test_parsing_steps passed and all action sequences are received correctly")
 
+
+
+    def test_rearranging_sequence(self):
+        '''
+        this test checks the rearranging sequence function of the action planning module
+        '''
+        self.received_sequences = []
+        # Subscribe to the topic where the action sequences are published
+        # 
+        for i, step in enumerate(self.dummy_steps):
+            # Publish the current step
+            rospy.loginfo(f"Publishing step: {step}")
+            self.step_publisher.publish(step)
+
+            # At one random step, publish a valid_command
+            if i == 1: 
+                rospy.loginfo("Publishing valid_command: wait 2 seconds")
+                self.valid_command_publisher.publish("wait 2 seconds")
+            
+            rospy.sleep(1)
+            
+            self.rate.sleep()
+        # Check if the received sequences match the dummy action sequences
+        if len(self.received_sequences) == len(self.dummy_action_seqences_valid_commands_sent):
+            for received, expected in zip(self.received_sequences, self.dummy_action_seqences_valid_commands_sent):
+                self.assertEqual(received, expected)
+        else:
+            self.fail(f"Number of received sequences ({len(self.received_sequences)}) does not match expected ({len(self.dummy_action_seqences_valid_commands_sent)}).")               
+
+        rospy.loginfo("test_rearranging_sequence passed and all action sequences are received correctly")
 
             
 
